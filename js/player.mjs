f74bce08b5c9b4ead275160385ab241862791d68
n1md7 <b.kodua@gmail.com>
2024-09-17 02:25:25 +0300

    fix: mul by bet

--- js/player.mjs
+++ js/player.mjs
@@ -44,6 +44,7 @@ export function Player(options) {
    * @readonly
    */
   this.addWin = (win) => {
+    win *= this.bet.get();
     this.credits.add(win);
     this.onWin(win);
     this.onUpdate(this.credits.get(), this.bet.get());
